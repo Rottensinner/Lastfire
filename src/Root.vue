761079ccb8311ed494fe6dd2984bd2db1f5819cf
Rottensinner <85820633+Rottensinner@users.noreply.github.com>
2026-09-22 10:06:01 +0200

    feat: mount settlement progression alongside game

---
+++ src/Root.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import App from "./App.vue";
+import SettlementProgressPanel from "./components/SettlementProgressPanel.vue";
+</script>
+
+<template>
+  <App />
+  <SettlementProgressPanel />
+</template>
